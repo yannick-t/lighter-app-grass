1dd0c5b7b9c620b96fadefbbb47f00897f705238
Yannick Tanner <yannick.tanner@student.kit.edu>
2016-06-27 15:28:50 +0200

    """

--- computeShaderGrass.glsl
+++ computeShaderGrass.glsl
@@ -18,8 +18,6 @@ layout(std140, binding = 2) uniform Grass
 {
 	CSGrassConstants grassConsts;
 };
-uniform vec3 frustumPlanePoints[6];
-uniform vec3 frustumPlaneNormals[6];
 
 #ifdef IN_CS
 layout(rgba32f, binding = 0) uniform image2D result;
@@ -27,13 +25,31 @@ layout(local_size_x = 32) in;
 
 void main()
 {
-	
-	
-	vec3 pos = frontCells[gl_LocalInvocationID.x];
-	
-	for(float i = 0; i < 32; i++) {
-		drawWorldPos(pos + i * grassConsts.FtBDirection, vec4(i / 31, 1.0, i / 31, 1.0));
-	 }
+	// Todo: Maybe only calc this once per Workgroup?
+	vec3 frustumPoints[8];
+	vec3 minFrustum;
+	vec3 maxFrustum;
+	vec3 frustumPlaneNormals[6]
+	vec4 temp = camera.ViewProj * vec4(gl_GlocalInvocationID.x / 32, gl_GlobalInvocationID.y / 32, 0.0, 1.0);
+	frustumPoints[0] = temp.xyz / temp.w;
+	temp = camera.ViewProj * vec4((gl_GlocalInvocationID.x + 1) / 32, (gl_GlobalInvocationID.y) / 32, 0.0, 1.0);
+	frustumPoints[1] = ntr.xyz / ntr.w;
+	temp = camera.ViewProj * vec4((gl_GlocalInvocationID.x + 1) / 32, (gl_GlobalInvocationID.y + 1) / 32, 0.0, 1.0);
+	frustumPoints[2] = ntr.xyz / ntr.w;
+	temp = camera.ViewProj * vec4((gl_GlocalInvocationID.x) / 32, (gl_GlobalInvocationID.y + 1) / 32, 0.0, 1.0);
+	frustumPoints[3] = ntr.xyz / ntr.w;
+	temp = camera.ViewProj * vec4(gl_GlocalInvocationID.x / 32, gl_GlobalInvocationID.y / 32, 1.0, 1.0);
+	frustumPoints[4] = temp.xyz / temp.w;
+	temp = camera.ViewProj * vec4((gl_GlocalInvocationID.x + 1) / 32, (gl_GlobalInvocationID.y) / 32, 1.0, 1.0);
+	frustumPoints[5] = ntr.xyz / ntr.w;
+	temp = camera.ViewProj * vec4((gl_GlocalInvocationID.x + 1) / 32, (gl_GlobalInvocationID.y + 1) / 32, 1.0, 1.0);
+	frustumPoints[6] = ntr.xyz / ntr.w;
+	temp = camera.ViewProj * vec4((gl_GlocalInvocationID.x) / 32, (gl_GlobalInvocationID.y + 1) / 32, 1.0, 1.0);
+	frustumPoints[7] = ntr.xyz / ntr.w;
+
+	for(int i = 0; i < 8; i++) {
+		
+	}
 }
 
 void drawWorldPos(vec3 pos, vec4 colour) {
